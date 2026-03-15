7b16e2062274542278e3e8f24305371ac343314a
Nicolay Wiig <nicowiig@gmail.com>
2026-03-15 20:34:19 +0100

    fix(kart): bytt til Esri World Street Map tiles

--- src/pages/StederPage.jsx
+++ src/pages/StederPage.jsx
@@ -405,8 +405,8 @@ export function StederPage() {
                     scrollWheelZoom={true}
                   >
                     <TileLayer
-                      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                      url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+                      attribution='Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ'
+                      url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}"
                     />
                     {mapAddrs.map(a => (
                       <CircleMarker
