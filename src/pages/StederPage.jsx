90fb63bf01209f17adc5196650c99f5713f1f1bd
Nicolay Wiig <nicowiig@gmail.com>
2026-03-15 20:52:54 +0100

    Map: switch to CartoDB Voyager tiles (lighter, better contrast)

--- src/pages/StederPage.jsx
+++ src/pages/StederPage.jsx
@@ -406,7 +406,7 @@ export function StederPage() {
                   >
                     <TileLayer
                       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-                      url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+                      url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
                       subdomains="abcd"
                       maxZoom={19}
                     />
